5f05b2cb58320a604e3dc8ce3006b9494402684d
Farhat Binte Azam <33277216+FarhatBuet14@users.noreply.github.com>
2020-08-15 02:11:39 +0600

    Put different color or type marks on images

--- codes/utills/put_marks.py
+++ codes/utills/put_marks.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 from PIL import Image
+import random
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -36,14 +37,46 @@ for i, info in enumerate(dataset.class_info):
 mark_dir = ROOT_DIR + "/data/marks/"
 marks = []
 pi_marks = []
-for file in os.listdir(mark_dir):
-    marks.append(cv.imread(mark_dir + file, 0))
-    pi_marks.append(Image.open(mark_dir + file))
-
-index_mark = 0
+marks_name = []
+for folder in os.listdir(mark_dir):
+    m, pi_m, n = [], [], []
+    for file in os.listdir(mark_dir + folder):
+        m.append(cv.imread(os.path.join(mark_dir, folder, file), 0))
+        pi_m.append(Image.open(os.path.join(mark_dir, folder, file)))
+        n.append(file)
+    marks.append(m)
+    pi_marks.append(pi_m)
+    marks_name.append(n)
 
 total_images = [info["path"].replace("2017/", "_person_area_face_masks/") for info in dataset.image_info]
 total_data = len(total_images)
+
+# --- Splitting Marks
+tot_mark = 5
+img_per_mark = (total_data // tot_mark) + 1
+details = []
+for i in range(tot_mark):
+    index_mark = i
+    in_start = (i*img_per_mark)
+    if(i==(tot_mark-1)): in_end = total_data
+    else: in_end = (i*img_per_mark + img_per_mark)
+    # print(f'Start - {in_start} End {in_end}')
+    for j in range(in_start, in_end):
+        m = [0, 1, 2, 3, 4]
+        m.remove(index_mark)
+        details.append((j,index_mark, random.randrange(1, 4), random.choice([True, False]), random.choice(m)))
+
+indices = []
+for detail in details:
+    ind = []
+    for i in range(detail[2]): ind.append(detail[1])
+    if(detail[3]) : ind.append(detail[4])
+    indices.append(ind)
+
+
+# from collections import Counter
+# Counter([i[2] for i in details])
+# Counter([i[3] for i in details])
 
 # # ---- Load past data
 # prepapred_data = np.load(COCO_DIR + 'marks_point_' + data_type + '.npz')
@@ -64,7 +97,7 @@ count = 0
 for i in range(len(total_images)):
 
     def mouse(event,x,y,flags,params):
-        global move_rectangle, BLUE, fg, bg, bgCopy, point
+        global move_rectangle, BLUE, bg, bgCopy, point
         #draw rectangle where x,y is rectangle center
         if event == cv.EVENT_LBUTTONDOWN:
             move_rectangle = True
@@ -87,11 +120,10 @@ for i in range(len(total_images)):
     
     move_rectangle = False
     BLUE = [255,0,0]
-
-    fg = marks[index_mark]
-    fg_pi = pi_marks[index_mark].copy()
     
-    rows, cols = fg.shape[:2]
+    rows, cols = (69, 55) # fg.shape[:2]
+    mark_index = indices[i]
+    print(f'{i+1} - Number of marks placed in image - {len(mark_index)}')
 
     cv.namedWindow('draw')
     cv.setMouseCallback('draw', mouse)
@@ -108,18 +140,26 @@ for i in range(len(total_images)):
         cv.imshow('draw', bg)
         k = cv.waitKey(1)
 
-        if k == 27 & 0xFF:
+        if (k == 27 & 0xFF) or ((len(point)//2)==len(mark_index)):
             points.append(point)
             paths.append(path)
             count += 1
             print("done - " + str(count))
             cv.destroyAllWindows()
             # Put the marks on selected points
-            for p in np.array(point).reshape((-1, 2)):
-                cox, coy = p
+            # fg = marks[index_mark]
+            # index_list = [i for i in range(0, len(pi_marks))]
+            # fg_pi = pi_marks[index_mark].copy()
+            
+            point = np.array(point).reshape((-1, 2))
+            for i, m in enumerate(mark_index):
+                fg_pi = pi_marks[m][random.choice([0, 1, 2, 3])].copy()
+                cox, coy = point[i]
                 bg_pi.paste(fg_pi, (cox, coy), fg_pi)
+            
             bg_pi.show()
             bg_pi.save(path.replace("_masks", "_masks_marks"))
+            
             point = []
             break
     
@@ -127,6 +167,7 @@ for i in range(len(total_images)):
             print("Exit.. Total - " + str(count))
             cv.destroyAllWindows()
             break
+
     if k == 32:
         break
 
