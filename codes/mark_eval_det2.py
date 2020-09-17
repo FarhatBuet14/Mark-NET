da2badb0885bf844c22022e8fb418df90fdc6201
Farhat Binte Azam <33277216+FarhatBuet14@users.noreply.github.com>
2020-09-17 18:32:08 +0600

    Evaluation on Test Images

---
+++ codes/mark_eval_det2.py
@@ -0,0 +1,162 @@
+# python mark_eval_det2.py
+
+import torch, torchvision
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+import glob
+import os
+import ntpath
+import numpy as np
+import cv2
+import random
+import itertools
+import pandas as pd
+from tqdm import tqdm
+import urllib
+import json
+import PIL.Image as Image
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor, DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.structures import BoxMode
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#ADFF02", "#8F00FF"]
+sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+rcParams['figure.figsize'] = 12, 8
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+
+#########################################    Start Codes    #########################################
+
+# --- Load Annotations
+data_type = "train" #val #test
+COCO_DIR = "/media/farhat/Farhat_SSD/MarkNET" + "/data/coco/"
+OUTPUT_DIR = "/media/farhat/Research/GitHub/Mark-NET/outputs/detectron2/"
+
+# --- Load Annotations
+df = pd.read_csv(COCO_DIR + "/marks_annotations.csv")
+train_df = pd.read_csv(COCO_DIR + "/marks_annotations_train.csv")
+test_df = pd.read_csv(COCO_DIR + "/marks_annotations_test.csv")
+
+classes = df.class_name.unique().tolist()
+
+
+# --- Dataset Dictionary for Training
+def create_dataset_dicts(df, classes):
+    dataset_dicts = []
+    for image_id, img_name in enumerate(df.file_name.unique()):
+        record = {}
+        image_df = df[df.file_name == img_name]
+        file_path = img_name
+        record["file_name"] = file_path
+        record["image_id"] = image_id
+        record["height"] = int(image_df.iloc[0].height)
+        record["width"] = int(image_df.iloc[0].width)
+        objs = []
+        for _, row in image_df.iterrows():
+            xmin = int(row.x_min)
+            ymin = int(row.y_min)
+            xmax = int(row.x_max)
+            ymax = int(row.y_max)
+            poly = [
+                (xmin, ymin), (xmax, ymin),
+                (xmax, ymax), (xmin, ymax)
+            ]
+            poly = list(itertools.chain.from_iterable(poly))
+            obj = {
+            "bbox": [xmin, ymin, xmax, ymax],
+            "bbox_mode": BoxMode.XYXY_ABS,
+            "segmentation": [poly],
+            "category_id": classes.index(row.class_name),
+            "iscrowd": 0
+            }
+            objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+    return dataset_dicts
+
+
+# --- Assign the Dictionary
+for d in ["train", "val"]:
+    DatasetCatalog.register("mark_" + d, lambda d=d: create_dataset_dicts(train_df if d == "train" else test_df, classes))
+    MetadataCatalog.get("mark_" + d).set(thing_classes=classes)
+statement_metadata = MetadataCatalog.get("mark_train")
+
+
+# --- Set our own Trainer (add Evaluator for the test set )
+class CocoTrainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            os.makedirs("coco_eval", exist_ok=True)
+            output_folder = "coco_eval"
+        return COCOEvaluator(dataset_name, cfg, False, output_folder)
+
+
+# --- Set the Configs
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_C4_3x.yaml"))
+
+cfg.DATASETS.TRAIN = ("mark_train",)
+cfg.DATASETS.TEST = ("mark_val",)
+cfg.DATALOADER.NUM_WORKERS = 4
+
+cfg.SOLVER.IMS_PER_BATCH = 1
+cfg.SOLVER.BASE_LR = 0.001
+cfg.SOLVER.WARMUP_ITERS = 1000
+cfg.SOLVER.MAX_ITER = 1500
+cfg.SOLVER.STEPS = (1000, 1500)
+cfg.SOLVER.GAMMA = 0.05
+
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 2
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(classes)
+cfg.TEST.EVAL_PERIOD = 500
+
+cfg.num_gpus = 1
+cfg.OUTPUT_DIR = OUTPUT_DIR + "2020-09-17 18_02 FasterRCNN"
+
+# --- Set Trainer
+trainer = CocoTrainer(cfg)
+
+# --- Evaluation Setup
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85
+predictor = DefaultPredictor(cfg)
+
+evaluator = COCOEvaluator("mark_val", cfg, False, output_dir=cfg.OUTPUT_DIR + "/validation/")
+val_loader = build_detection_test_loader(cfg, "mark_val")
+inference_on_dataset(trainer.model, val_loader, evaluator)
+
+# ---- Test on Images
+Annotation_folder = cfg.OUTPUT_DIR + "/annotated_results"
+os.makedirs(Annotation_folder, exist_ok=True)
+test_image_paths = test_df.file_name.unique()
+for clothing_image in test_image_paths:
+    file_path = clothing_image
+    im = cv2.imread(clothing_image)
+    outputs = predictor(im)
+    v = Visualizer(
+    im[:, :, ::-1],
+    metadata=statement_metadata,
+    scale=1.,
+    instance_mode=ColorMode.IMAGE
+    )
+    instances = outputs["instances"].to("cpu")
+    # instances.remove('pred_masks')
+    v = v.draw_instance_predictions(instances)
+    result = v.get_image()[:, :, ::-1]
+    file_name = ntpath.basename(clothing_image)
+    write_res = cv2.imwrite(f'{Annotation_folder}/{file_name}', result)
+
+print("Finished Evaluation...")
